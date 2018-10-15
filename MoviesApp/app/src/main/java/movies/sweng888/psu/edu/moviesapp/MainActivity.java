9fcbc80793be16698c010c111a6b80685dc52aeb
G2-GV-EZT157L1\ezt157 <ezt157@psu.edu>
2018-10-15 16:57:58 -0400

    Code cleaning.

--- MoviesApp/app/src/main/java/movies/sweng888/psu/edu/moviesapp/MainActivity.java
+++ MoviesApp/app/src/main/java/movies/sweng888/psu/edu/moviesapp/MainActivity.java
@@ -8,28 +8,14 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewMovieRating;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // set the content of activity to use activity_main.xml layout file.
         setContentView(R.layout.activity_main);
 
-        // sets the reference to the layout element that will be handled by java code
-        mTextViewMovieRating = (TextView) findViewById(R.id.movie_rating);
-
-        mTextViewMovieRating.setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(this, "Go to next page.", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, RatingActivity.class);
-        startActivity(intent);
     }
 }
