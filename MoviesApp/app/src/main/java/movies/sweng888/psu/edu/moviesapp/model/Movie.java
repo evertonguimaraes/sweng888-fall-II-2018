21b1a8a511090823d1caf1107e1e129f8d224282
G2-GV-EZT157L1\ezt157 <ezt157@psu.edu>
2018-10-30 19:52:20 -0400

    Added listView to the MoviesApp.

---
+++ MoviesApp/app/src/main/java/movies/sweng888/psu/edu/moviesapp/model/Movie.java
@@ -0,0 +1,51 @@
+package movies.sweng888.psu.edu.moviesapp.model;
+
+public class Movie {
+
+    private String title;
+    private String category;
+    private String rating;
+    private int year;
+
+    public Movie(String title, int year){
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return this.title+" "+String.valueOf(this.year);
+    }
+}
